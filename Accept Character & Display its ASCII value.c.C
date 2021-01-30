93d7feaf201e2e4f16c1658f94e2b72866a18289
Gayatrishewale20 <78014554+Gayatrishewale20@users.noreply.github.com>
2021-01-30 14:17:57 +0530

    Add files via upload

---
+++ Accept Character & Display its ASCII value.c.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch='10';
+    printf("\n character To ASCII conversion =>\n\n");
+    printf("\n Enter a character to get its ASCII value");
+    ch=getche();
+    printf("\n\n ASCII value of character %c is=%d",ch,ch);
+    printf("\n\n Thanks for using our service...");
+    getch();
+    return 0;
+
+}
